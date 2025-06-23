9a93f6d9d8d0fd913879fdbf888e2acdd13f4b20
Cristian1274 <140935627+Cristian1274@users.noreply.github.com>
2025-06-23 13:31:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get('/project/:id', (req, res) => {
   }
 });
 
-app.listen(4000, () => {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`API escuchando en http://localhost:${PORT}`);
+});
+
   console.log('API escuchando en http://localhost:3000');
 });
